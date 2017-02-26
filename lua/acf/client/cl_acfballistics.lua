079fd1481ce8cd7672e3144373200499500a4c00
Stephen Jobstein <steejerbs@gmail.com>
2017-02-25 20:47:24 -0800

    Merge update

--- lua/acf/client/cl_acfballistics.lua
+++ lua/acf/client/cl_acfballistics.lua
@@ -10,13 +10,13 @@ end
 hook.Add("Tick", "ACF_ManageBulletEffects", ACF_ManageBulletEffects)
 
 function ACF_SimBulletFlight( Bullet, Index )
-
 	local Time = CurTime()
 	local DeltaTime = Time - Bullet.LastThink
 	
-	local Drag = Bullet.SimFlight:GetNormalized() * (Bullet.DragCoef * Bullet.SimFlight:LengthSqr()) / ACF.DragDiv
+	local Drag = Bullet.SimFlight:GetNormalized() * (Bullet.DragCoef * Bullet.SimFlight:LengthSqr())/ACF.DragDiv
 	--print(Drag)
-
+	--debugoverlay.Cross(Bullet.SimPos,3,15,Color(255,255,255,32), true)
+	Bullet.SimPosLast = Bullet.SimPos
 	Bullet.SimPos = Bullet.SimPos + (Bullet.SimFlight * ACF.VelScale * DeltaTime)		--Calculates the next shell position
 	Bullet.SimFlight = Bullet.SimFlight + (Bullet.Accel - Drag)*DeltaTime			--Calculates the next shell vector
 	
